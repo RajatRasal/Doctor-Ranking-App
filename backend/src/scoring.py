583895375950944815765226b565f525ebcc108f
RajatRasal <yugiohrajat1@gmail.com>
2019-09-05 22:57:50 +0100

    Project structure and .gitignore

---
+++ backend/src/scoring.py
@@ -0,0 +1,6 @@
+"""
+Scoring algorithms for doctors and sorting algorithm based on score.
+"""
+def calculate_score():
+    print('TO DO')
+    return False
---
+++ backend/tests/test_doctor_rank.py
@@ -0,0 +1,11 @@
+"""
+Tests for the doctor ranking algorithm.
+"""
+import mock
+import pytest
+
+from src.scoring import calculate_score 
+
+def test_calculate_score():
+    calculate_score()
+    assert False
