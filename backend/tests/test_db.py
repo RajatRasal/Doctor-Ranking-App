dfbf485c165afb9b4ec0c3c5a469c663a86320df
RajatRasal <yugiohrajat1@gmail.com>
2019-09-13 23:01:32 +0100

    Add tests for creating new tables

--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -4,8 +4,9 @@ delete from the database.
 """
 import mock
 import pytest
-from sqlalchemy import MetaData, Column, Integer, inspect
+from sqlalchemy import MetaData, Column, Integer, Table, inspect
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
 
 from src.model.tables import create_table_if_not_exists, drop_table_if_exists
 
@@ -17,6 +18,7 @@ class TestPostgresCRUD:
         Drops all tables in test db instance.
         """
         print('SETUP')
+        self.engine = test_db_conn
         self.conn = test_db_conn.connect()
         self.metadata = MetaData(self.conn)
         self.trans = self.conn.begin()
@@ -25,44 +27,60 @@ class TestPostgresCRUD:
         self.trans.rollback()
         print('TEARDOWN')
 
-    def test_create_new_table(self):
+    def test_create_new_table_and_returns_table_obj(self):
         """
         Inserts new table into db.
         """
-        tablename = 'diseases'
+        tablename = 'test_table'
 
-        create_table_if_not_exists(tablename, self.conn, Column('id', Integer))
+        table = create_table_if_not_exists(tablename, self.conn, 
+            Column('id', Integer))
 
         inspector = inspect(self.conn)
         new_table_names = inspector.get_table_names()
 
-        assert len(new_table_names) == 1
-        assert new_table_names[0] == tablename
+        assert tablename in new_table_names
+        assert isinstance(table, Table)
+        assert [str(col) for col in table.columns] == ['test_table.id']
 
-    def test_no_col_given_for_create_new_table_return_false(self, test_db_conn):
+    def test_table_not_created_if_name_already_exists_and_returns_none(self):
         """
-        Inserts new table into db.
+        Inserts new table into db, adds data to that table, then tries to create
+        the same table again. The second table should not be created.
         """
-        # metadata = MetaData(test_db_conn)
-        # res = create_table_if_not_exists('diseases', test_db_conn, []) 
-        assert False
+        tablename = 'test_table'
+        test_table = create_table_if_not_exists(tablename, self.conn, 
+            Column('test_id', Integer))
+
+        insert_one = test_table.insert().values(test_id=1)
+        self.conn.execute(insert_one)
+
+        select_all = select([test_table])
+        res = self.conn.execute(select_all).fetchall()
+        assert len(res) == 1
+
+        table = create_table_if_not_exists(tablename, self.conn,
+            Column('test_id', Integer))
+
+        res = self.conn.execute(select_all).fetchall()
+        assert len(res) == 1
+        assert table == None
+
+    def test_no_col_given_for_create_new_table_return_false(self):
+        """
+        No action is performed if we attempt to create a table with no columns. 
+        """
+        assert False 
 
 
-    def test_table_not_created_if_name_already_exists_and_returns_false(self):
-        """
-        No new table inserted if there is already a table with the same name in
-        the database.
-        """
-        assert False
-
-    def test_drop_table_if_exists_and_return_true(self):
+    def NOT_test_drop_table_if_exists_and_return_true(self):
         """
         Drops table from db if it exists.
         """
-        assert False
+        assert True
 
-    def test_table_not_deleted_if_not_exists_and_return_false(self):
+    def NOT_test_table_not_deleted_if_not_exists_and_return_false(self):
         """
         No action is performed if we attempt to drop a non-existent table. 
         """
-        assert False
+        assert True
