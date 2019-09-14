d1a5f37d7de652fdb69e0c739da3256bf61878ac
RajatRasal <yugiohrajat1@gmail.com>
2019-09-14 01:06:22 +0100

    Written tests for input checking for insert into function

--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -8,7 +8,8 @@ from sqlalchemy import MetaData, Column, Integer, Table, inspect
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select
 
-from src.model.tables import create_table_if_not_exists, drop_table_if_exists
+from src.model.tables import create_table_if_not_exists, \
+        insert_unique_records_to_table
 
 class TestPostgresCRUD:
 
@@ -20,10 +21,13 @@ class TestPostgresCRUD:
         print('SETUP')
         self.engine = test_db_conn
         self.conn = test_db_conn.connect()
+        self.session = sessionmaker(self.conn)()
+        self.session.begin_nested()
         self.metadata = MetaData(self.conn)
         self.trans = self.conn.begin()
         print('YIELD TO:', request.function.__name__)
         yield 
+        self.session.rollback()
         self.trans.rollback()
         print('TEARDOWN')
 
@@ -57,12 +61,14 @@ class TestPostgresCRUD:
 
         select_all = select([test_table])
         res = self.conn.execute(select_all).fetchall()
+
         assert len(res) == 1
 
         table = create_table_if_not_exists(tablename, self.conn,
             Column('test_id', Integer))
 
         res = self.conn.execute(select_all).fetchall()
+
         assert len(res) == 1
         assert table == None
 
@@ -78,6 +84,31 @@ class TestPostgresCRUD:
 
         assert table == None
         assert 'test_table' not in table_names
+
+    def test_no_records_given_for_inserting_returns_false(self):
+        """
+        No action is performed when no records are input to be inserted into the
+        table and False is returned.
+        """
+        tablename = 'test_table'
+        table = create_table_if_not_exists(tablename, self.conn,
+            Column('test_id', Integer))
+
+        res = insert_unique_records_to_table(tablename, self.conn, [])
+
+        query_res = self.session.execute(table.select())
+
+        assert not list(query_res)
+        assert res == False 
+
+    def test_no_action_when_no_tablename_is_given_for_inserting(self):
+        """
+        No action is performed when there is no tablename given for inserting
+        unique records into.
+        """
+        res = insert_unique_records_to_table('', self.conn, [['x']])
+
+        assert not res
 
 
     def NOT_test_drop_table_if_exists_and_return_true(self):
