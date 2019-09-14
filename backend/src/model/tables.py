feb3ad2f3b132ae370bbbb46c26005b4f3ebd865
RajatRasal <yugiohrajat1@gmail.com>
2019-09-14 01:21:07 +0100

    1) Passing basic input validation tests for insert into function 2) Created schemas for diseases, parameters and dis_param relationship table

--- backend/src/model/tables.py
+++ backend/src/model/tables.py
@@ -4,7 +4,7 @@ equivalent to DDL only using SQLAlchemy's ORM instead of SQL.
 """
 import sqlalchemy
 from sqlalchemy import MetaData, Table, Integer, String, Column, ForeignKey, \
-    inspect
+    CheckConstraint, inspect
 
 from db_connection import get_db_connection
 
@@ -14,7 +14,7 @@ def create_table_if_not_exists(name, db_conn, *cols):
     that a table of the same name does not already exist.
     """
     # Exit if no columns given
-    if not cols:
+    if not cols or not db_conn:
         return None
 
     # Reflect db connetion to get existing schema from database
@@ -28,32 +28,38 @@ def create_table_if_not_exists(name, db_conn, *cols):
     except sqlalchemy.exc.InvalidRequestError:
         return None
 
-# def drop_table_if_exists(name, db_conn):
-#     pass
+def drop_table_if_exists(name, db_conn):
+    if not (name and db_conn):
+        return False
 
-# def add_unique_records_to_table(table, records):
-#     pass
+    return True
+
+def insert_unique_records_to_table(table, db_conn, records):
+    if not (table and db_conn and records):
+        return False
+    return True
 
 if __name__ == "__main__":
     engine = get_db_connection()
 
+    # Drop all tables if they exist
+
     # Create diseases table
     diseases = create_table_if_not_exists('diseases', engine,
         Column('id', Integer, primary_key=True),
-        Column('type', String))
+        Column('type', String, nullable=False))
 
     # Create Parameters table
     parameters = create_table_if_not_exists('parameters', engine,
         Column('id', Integer, primary_key=True),
-        Column('type', String))
+        Column('type', String, nullable=False))
 
-    # Create Disase-Parameters link table
+    # Create Disease-Parameters relationship table
     dis_param_link_name = 'disease_params_importance'
-    disease_params_importance = create_table_if_not_exists(dis_param_link_name,
-        engine,
-        Column('disease_id', Integer, ForeignKey('diseases.id'), nullable=False),
-        Column('parameter_id', Integer, ForeignKey('parameters.id'), nullable=False),
-        Column('importance', Integer, nullable=False))
+    disease_params_importance = create_table_if_not_exists(dis_param_link_name, engine,
+        Column('disease_id', Integer, ForeignKey('diseases.id'), nullable=False, primary_key=True),
+        Column('parameter_id', Integer, ForeignKey('parameters.id'), nullable=False, primary_key=True),
+        Column('importance', Integer, CheckConstraint('importance >= 0 and importance <= 5'), nullable=False))
 
     inspector = inspect(engine.connect())
     print(f'Existing tables: {inspector.get_table_names()}')
