3ceac831bb50d04e991c77756f2348aa77d03d6d
RajatRasal <yugiohrajat1@gmail.com>
2019-09-24 16:13:47 +0100

    Add disease selection react form with bootstrap styling.

--- src/static/scripts/like_button.js
+++ src/static/scripts/like_button.js
@@ -1,18 +1,52 @@
 'use strict';
 
-const domContainer = document.querySelector('#disease_selection_form');
+const domContainer = document.querySelector('#engine');
 
 function DiseasesList(props) {
   const diseases = props.diseases;
-  const listItems = diseases.map((disease, index) =>
-    <option key={index} value={disease}>{disease}</option>
-  );
   return (
     <select>{listItems}</select>
   );
 }
 
-fetch('/diseases')
+class DiseaseSelectionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.diseases = props.diseases;
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    const diseaseListItems = this.diseases.map((disease, index) =>
+      <option key={index} value={disease}>{disease}</option>
+    );
+    return (
+      <div>
+        <h1 className="mt-5">Select Disease</h1>
+        <p className="lead">These are all the diseases present in the database. To add more, click on the <span className="font-italic">Add/Update Diseases Info</span> tab.</p> 
+        <form onSubmit={this.handleSubmit}>
+	  <div className="input-group">
+            <select className="custom-select">
+              <option selected>Choose...</option> 
+              {diseaseListItems}
+            </select>
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary" type="submit">Next</button>
+            </div>
+	  </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const diseases = fetch('/diseases')
   .then(response => {
     if (response.status != 200) {
       console.log("Server Error: " + response.text());
@@ -22,7 +56,7 @@ fetch('/diseases')
       .then(data => {
         console.log(data['diseases']);
         ReactDOM.render(
-          <DiseasesList diseases={data['diseases']}/>,
+          <DiseaseSelectionForm diseases={data['diseases']}/>,
           domContainer
         );
       });
