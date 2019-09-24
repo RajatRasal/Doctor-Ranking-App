60456f64901126d64c2d0d022cefd9f17bb95f78
RajatRasal <yugiohrajat1@gmail.com>
2019-09-24 02:59:08 +0100

    Add random number to prevent JS caching

--- src/static/scripts/like_button.js
+++ src/static/scripts/like_button.js
@@ -1,7 +1,45 @@
 'use strict';
 
-// const e = React.createElement;
+const e = React.createElement;
 const base_url = 'https://localhost:5000/diseases';
+
+/*
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+*/
 
 /*
 class LikeButton extends React.Component {
@@ -24,6 +62,16 @@ class LikeButton extends React.Component {
 }
 */
 
+const domContainer = document.querySelector('#like_button_container');
+
+function DiseasesList(props) {
+  const diseases = props.diseases;
+  const listItems = diseases.map((disease) =>
+    <li key={disease}>{disease}</li>
+  );
+
+  return (<ul>{listItems}</ul>);
+}
 
 fetch('/diseases')
   .then(response => {
@@ -31,7 +79,15 @@ fetch('/diseases')
       console.log("Server Error: " + response.text());
       return;
     }
-    console.log(response.json()['diseases'])
+    response.json()
+      .then(data => {
+        console.log('THERE');
+        console.log(data['diseases']);
+        ReactDOM.render(
+          <DiseasesList diseases={data['diseases']}/>,
+          domContainer
+        );
+      });
   })
   .catch(error => console.log('Fetch error: ' + error)
   );
